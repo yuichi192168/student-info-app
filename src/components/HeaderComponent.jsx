a0874a27732b432b0d792229dcfa3964710dfcb2
yuichi192168 <yuichi192168@gmail.com>
2026-02-16 11:16:59 +0800

    add api error

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -7,7 +7,9 @@ function HeaderComponent() {
   return (
     <header className="site-header">
       <div className="brand">
-        <span className="brand-mark">SI</span>
+        <span className="brand-mark">
+          <img className="brand-logo" src="/student.png" alt="Student logo" />
+        </span>
         <div>
           <p className="brand-title">Student Info App</p>
           <p className="brand-subtitle">Simple roster insights</p>
